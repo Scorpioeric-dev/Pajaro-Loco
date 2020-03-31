7797e01006b5a1618fe700f2433e000d2d682dbe
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-03-31 08:35:24 -0600

    start

--- pajaro-loco/game.js
+++ pajaro-loco/game.js
@@ -1,3 +1,21 @@
 // JAVASCRIPT CODE //
-const cvs = document.getElementById('pajaro')
-cvs.getContext('2d')
+const cvs = document.getElementById("canvas");
+const ctx = cvs.getContext("2d");
+let frames = 0;
+const sprite = new Image()
+sprite.src = "img/sprite.png"
+
+function draw() {
+  //This is the background of entire image
+  ctx.fillStyle = "#70c5ce";
+  ctx.fillRect(0, 0, cvs.width, cvs.height);
+  ctx.drawImage(sprite,sX,sY,sWidth,sHeight,dX,dY,dWidth,dHeight)
+}
+function loop() {
+  update();
+  draw();
+  frames++;
+
+  requestAnimationFrame(loop);
+}
+loop();
